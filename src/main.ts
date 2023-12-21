1d63ecf0ede56a6b72496d3f313c3767c83b4e70
Loic Diridollou <loic.diridollou@gmail.com>
2023-12-20 19:59:22 -0800

    Test

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-const core = require('@actions/core')
-const { wait } = require('./wait')
+import * as core from '@actions/core'
+import { wait } from './wait'
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
-async function run() {
+export async function run(): Promise<void> {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const ms: string = core.getInput('milliseconds')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Waiting ${ms} milliseconds ...`)
@@ -21,10 +21,6 @@ async function run() {
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message)
   }
-}
-
-module.exports = {
-  run
 }
--- src/wait.ts
+++ src/wait.ts
@@ -1,10 +1,9 @@
 /**
  * Wait for a number of milliseconds.
- *
- * @param {number} milliseconds The number of milliseconds to wait.
+ * @param milliseconds The number of milliseconds to wait.
  * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
  */
-async function wait(milliseconds) {
+export async function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
     if (isNaN(milliseconds)) {
       throw new Error('milliseconds not a number')
@@ -13,5 +12,3 @@ async function wait(milliseconds) {
     setTimeout(() => resolve('done!'), milliseconds)
   })
 }
-
-module.exports = { wait }
