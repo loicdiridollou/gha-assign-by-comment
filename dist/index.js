dba6b2bed25c3b59158310e669419182c49ce9ad
Loic Diridollou <loic.diridollou@gmail.com>
2023-12-22 13:46:40 -0800

    Test

--- dist/index.js
+++ dist/index.js
@@ -2756,19 +2756,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = void 0;
 const core = __importStar(__nccwpck_require__(186));
-const wait_1 = __nccwpck_require__(259);
 const fs_1 = __importDefault(__nccwpck_require__(147));
 async function run() {
     try {
-        const ms = core.getInput("milliseconds");
-        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        core.debug(`Waiting ${ms} milliseconds ...`);
-        // Log the current timestamp, wait, then log the new timestamp
-        core.debug(new Date().toTimeString());
         const GITHUB_EVENT_PATH = process.env.GITHUB_EVENT_PATH;
-        console.log(JSON.parse(fs_1.default.readFileSync(GITHUB_EVENT_PATH, "utf-8")));
-        await (0, wait_1.wait)(parseInt(ms, 10));
-        core.debug(new Date().toTimeString());
+        let eventFile = JSON.parse(fs_1.default.readFileSync(GITHUB_EVENT_PATH, "utf-8"));
+        console.log(eventFile.comment.issue_url);
+        console.log(eventFile.comment.body);
+        console.log(eventFile.user.login);
         // Set outputs for other workflow steps to use
         core.setOutput("time", new Date().toTimeString());
     }
@@ -2779,31 +2774,6 @@ async function run() {
     }
 }
 exports.run = run;
-
-
-/***/ }),
-
-/***/ 259:
-/***/ ((__unused_webpack_module, exports) => {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-/**
- * Wait for a number of milliseconds.
- * @param milliseconds The number of milliseconds to wait.
- * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-    return new Promise((resolve) => {
-        if (isNaN(milliseconds)) {
-            throw new Error("milliseconds not a number");
-        }
-        setTimeout(() => resolve("done!"), milliseconds);
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from "@actions/core";
-import { wait } from "./wait";
 import fs from "fs";
 
 export async function run(): Promise<void> {
